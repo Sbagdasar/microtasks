b9bbb28f2f0b4b4ad439e57285fd25da79d159cd
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-22 17:39:19 +0300

    added Header, Body, Footer components

---
+++ src/site/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+type FooterPropsType = {
+    titleForFooter:string
+}
+export const Footer: React.FC<FooterPropsType> = (props) => {
+    return (
+        <div>
+            {props.titleForFooter}
+        </div>
+    );
+};
+
