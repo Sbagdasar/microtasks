c00f200e8046b3d0a42c1c48f79ea9c0c1ffb461
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-22 18:51:18 +0300

    added button

--- src/site/Body.tsx
+++ src/site/Body.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+
 export type BodyPropsType = {
     titleForBody: string,
-    topCars:Array<topCarsPropsType>
+    topCars: Array<topCarsPropsType>
 }
-export type topCarsPropsType ={
-        manufacturer:string,
-        model:string
+export type topCarsPropsType = {
+    manufacturer: string,
+    model: string
 }
-export const Body:React.FC<BodyPropsType> = (props) => {
+export const Body: React.FC<BodyPropsType> = (props) => {
     return (
         <div>
-           <div>{props.titleForBody}</div>
-            <div>{props.topCars.map(el=>{
-                return(
-                    <ul>{el.model} - {el.manufacturer}</ul>
-                )
-            })}</div>
+            <div>{props.titleForBody}</div>
+            <table>
+                <tbody>
+                    <tr>
+                        {props.topCars.map((el, index) => {
+                            return (
+                                <th key={index + 1}>
+                                    {el.model} - {el.manufacturer} - {index+1}
+                                </th>
+                            )
+                        })}
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
--- src/site/Footer.tsx
+++ src/site/Footer.tsx
@@ -3,9 +3,19 @@ type FooterPropsType = {
     titleForFooter:string
 }
 export const Footer: React.FC<FooterPropsType> = (props) => {
+    const myFirstSubscriber = ()=>{
+        console.log("hello Chesya")
+    }
+    const mySecondSubscriber=()=>{
+        console.log("I am Sam")
+    }
     return (
         <div>
             {props.titleForFooter}
+            <div>
+                <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
+                <button onClick={mySecondSubscriber}>MyYouTubeChanel-2</button>
+            </div>
         </div>
     );
 };
